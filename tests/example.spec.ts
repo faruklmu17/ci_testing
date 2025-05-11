ed4fda436d6d3744e5502cab83a8fd919350579d
faruklmu17 <farukqmul@gmail.com>
2025-05-11 08:42:57 -0400

    three tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -19,7 +19,12 @@ test('get started link1', async ({ page }) => {
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
 
+test('has title2', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
 
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
 
 
 
