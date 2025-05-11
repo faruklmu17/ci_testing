e9223adc5a6d897ff7695f0c4dece3f1e82d4950
faruklmu17 <farukqmul@gmail.com>
2025-05-11 08:51:16 -0400

    one test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -8,37 +8,3 @@ test('has title1', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
-
-test('get started link1', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
-
-test('has title2', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-
-
-
-
-  
-
-
-
-
-
-
-  
-
-
-
-
