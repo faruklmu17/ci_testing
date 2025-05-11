db893e62a46c3ae2aee79bd62ea33af14c0a03c8
faruklmu17 <farukqmul@gmail.com>
2025-05-11 09:07:16 -0400

    updated tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -16,16 +16,3 @@ test('has title2', async ({ page }) => {
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('has title3', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('has title4', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
