a8e12855426b9fb8c0c2674073ddce123062e6af
faruklmu17 <farukqmul@gmail.com>
2025-05-11 02:50:05 -0400

    changes to test numbers

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -53,12 +53,7 @@ test('has title5', async ({ page }) => {
 });
 
   
-test('has title6', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
 
 
 
