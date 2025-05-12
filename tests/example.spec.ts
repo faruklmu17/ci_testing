b509d0005e765cb23a5e36f4f0ab90189bd5fe53
faruklmu17 <farukqmul@gmail.com>
2025-05-12 11:58:18 -0400

    updated tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -16,3 +16,10 @@ test('has title2', async ({ page }) => {
   await expect(page).toHaveTitle(/Playwright/);
 });
 
+test('has title3', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
